d407cbc19a0822b57e2fe223f8ec3ae750180483
Unelmat <144540668+Unelmat@users.noreply.github.com>
2023-12-06 10:10:23 +0300

    first comment

--- app/src/main/java/com/example/myapplication/MainActivity5.kt
+++ app/src/main/java/com/example/myapplication/MainActivity5.kt
@@ -3,19 +3,15 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main5)
     }
-
     fun OnClickBack(view: View){
-        val intent = Intent(this@MainActivity, MainActivity2::class.java)
+        val intent = Intent(this@MainActivity5, MainActivity2::class.java)
         startActivity(intent)
     }
-
 }
